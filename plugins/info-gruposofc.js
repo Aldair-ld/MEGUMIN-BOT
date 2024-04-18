8ecf9fc68959d7bc90308a9088e2a2d13282c424
Aldair013 <166980209+Aldair013@users.noreply.github.com>
2024-04-17 18:50:40 -0600

    Actualizar info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -4,18 +4,23 @@ let fakemek = {key: {participant: "0@s.whatsapp.net","remoteJid": "0@s.whatsapp.
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let str = `*ESTOS SON LOS GRUPOS OFICIALES*
  𝐌𝐢 𝐂𝐚𝐧𝐚𝐥 𝐨𝐟𝐢𝐜𝐢𝐚𝐥
- *${canalofc}*\n
+ *https://whatsapp.com/channel/0029VaWZeyJJENxvi5Q6OQ2q*
+
 ⏤͟͟͞͞★꙲⃝͟🌹 ─━━━━┉❈⏤͟͟͞͞★꙲⃝͟🌻❈┉━━━━─
- 𝐆𝐫𝐮𝐩𝐨 𝐎𝐟𝐢𝐜𝐢𝐚𝐥 𝟓
-*${nnnttt}*\n
+ 𝐆𝐫𝐮𝐩𝐨 𝐎𝐟𝐢𝐜𝐢𝐚𝐥
+*https://chat.whatsapp.com/CZHOvEJpekOH6qMfwJXTXz*
+
 ⏤͟͟͞͞★꙲⃝͟🌹 ─━━━━┉❈⏤͟͟͞͞★꙲⃝͟🌻❈┉━━━━─
- 𝑭𝒂𝒏𝒕𝒂𝒔𝒚𝑩𝒐𝒕-𝑴𝑫 
-*${nnnttt1}*\n
+
+*BILL -BOT*
+*https://whatsapp.com/channel/0029VaWZeyJJENxvi5Q6OQ2q*
+
 ⏤͟͟͞͞★꙲⃝͟🌹 ─━━━━┉❈⏤͟͟͞͞★꙲⃝͟🌻❈┉━━━━─
- 𝑭𝒂𝒏𝒕𝒂𝒔𝒚𝑩𝒐𝒕-𝑴𝑫 
-⏤͟͟͞͞★꙲⃝͟🌹 *${nnnttt2}*\n
-─━━━━┉❈⏤͟͟͞͞★꙲⃝͟🌻❈┉━━━━─
-*hola :D*`
+
+*BILL - BOT*
+⏤͟͟͞͞★꙲⃝͟🌹 *https://chat.whatsapp.com/CZHOvEJpekOH6qMfwJXTXz*
+
+─━━━━┉❈⏤͟͟͞͞★꙲⃝͟🌻❈┉━━━━─`
 await conn.sendFile(m.chat, media, 'fantasy.mp4', str, fakemek)}
 
 handler.command = /^linkgc|grupos|gruposfantasy|groupofc$/i
