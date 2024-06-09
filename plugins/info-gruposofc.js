bec22bd005b5a7a13616307c833b24fa358ec0ec
Aldair-ld <frankaldair29@gmail.com>
2024-06-09 17:58:50 -0500

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -4,23 +4,21 @@ let fakemek = {key: {participant: "0@s.whatsapp.net","remoteJid": "0@s.whatsapp.
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let str = `*ESTOS SON LOS GRUPOS OFICIALES*
  𝐌𝐢 𝐂𝐚𝐧𝐚𝐥 𝐨𝐟𝐢𝐜𝐢𝐚𝐥
- *https://whatsapp.com/channel/0029VaWZeyJJENxvi5Q6OQ2q*
+ *https://whatsapp.com/channel/0029VafZvB6J3jv3qCnqNu3x*
 
-⏤͟͟͞͞★꙲⃝͟🌹 ─━━━━┉❈⏤͟͟͞͞★꙲⃝͟🌻❈┉━━━━─
- 𝐆𝐫𝐮𝐩𝐨 𝐎𝐟𝐢𝐜𝐢𝐚𝐥
-*https://chat.whatsapp.com/CZHOvEJpekOH6qMfwJXTXz*
 
-⏤͟͟͞͞★꙲⃝͟🌹 ─━━━━┉❈⏤͟͟͞͞★꙲⃝͟🌻❈┉━━━━─
+♡ 𝐌𝐄𝐆𝐔𝐌𝐈𝐍 - 𝐁𝐎𝐓 ♡
+*https://chat.whatsapp.com/LErRdiLdyYa7Xq5FwCd2Y4*
 
-*BILL -BOT*
-*https://whatsapp.com/channel/0029VaWZeyJJENxvi5Q6OQ2q*
 
-⏤͟͟͞͞★꙲⃝͟🌹 ─━━━━┉❈⏤͟͟͞͞★꙲⃝͟🌻❈┉━━━━─
+
+♡ 𝐌𝐄𝐆𝐔𝐌𝐈𝐍 - 𝐁𝐎𝐓 ♡
+*https://whatsapp.com/channel/0029VafZvB6J3jv3qCnqNu3x*
+
+
 
 *BILL - BOT*
-⏤͟͟͞͞★꙲⃝͟🌹 *https://chat.whatsapp.com/CZHOvEJpekOH6qMfwJXTXz*
-
-─━━━━┉❈⏤͟͟͞͞★꙲⃝͟🌻❈┉━━━━─`
+⏤͟͟͞͞★꙲⃝͟🌹 *https://chat.whatsapp.com/LErRdiLdyYa7Xq5FwCd2Y4*`
 await conn.sendFile(m.chat, media, 'fantasy.mp4', str, fakemek)}
 
 handler.command = /^linkgc|grupos|gruposfantasy|groupofc$/i
