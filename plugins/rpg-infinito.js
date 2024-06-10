0b94945919689a2e9d355ecf027e2222d8a2cd36
Aldair-ld <frankaldair29@gmail.com>
2024-06-10 00:07:14 -0500

    Update rpg-infinito.js

--- plugins/rpg-infinito.js
+++ plugins/rpg-infinito.js
@@ -3,7 +3,7 @@ let { MessageType } = (await import(global.baileys)).default
 //SOLO USA SI ERES EL/LA PROPIETARIO(A) DEL BOT PARA TENER TODO ILIMITADO O USA EL COMANDO PREMIUM Jajaj
 let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender]
-        conn.reply(m.chat, `*ÉXITO!!*`, m)
+        conn.reply(m.chat, `*ÉXITO AHORA TIENE TODO LOS RECURSOS ILIMITADOS!!*`, m)
         global.db.data.users[m.sender].money = Infinity
         global.db.data.users[m.sender].limit = Infinity
         global.db.data.users[m.sender].level = Infinity
