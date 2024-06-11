769af3ea27c7492310488a95221ada634de1c873
Aldair-ld <frankaldair29@gmail.com>
2024-06-11 13:54:37 -0500

    Update jadibot-lista.js

--- plugins/jadibot-lista.js
+++ plugins/jadibot-lista.js
@@ -27,7 +27,7 @@ return resultado;
 const message = users.map((v, index) => `(${index + 1})\n🌷 wa.me/${v.user.jid.replace(/[^0-9]/g, '')}?text=${usedPrefix}estado\n* 𝙽𝙾𝙼𝙱𝚁𝙴 :* *${v.user.name || '-'}*\n*𝚃𝙸𝙴𝙼𝙿𝙾 𝙰𝙲𝚃𝙸𝚅𝙾 :* ${ v.uptime ? convertirMsADiasHorasMinutosSegundos(Date.now() - v.uptime) : "Desconocido"}`).join('\n\n••••••••••••••••••••••••••••••••••••\n\n');
   const replyMessage = message.length === 0 ? '*NO HAY SUB BOTS DISPONIBLE. VERIFIQUE MÁS TARDE.*' : message;
 const totalUsers = users.length;
-const responseMessage = `*CARGANDO LA LISTA DE BOTS ACTIVOS\n █▒▒▒▒▒▒▒▒▒10%\n ████▒▒▒▒▒▒30%\n █████▒▒▒▒▒50%\n ████████▒▒80%\n ██████████100%\n\n *ESTA ES LA LISTA DE SUBOTS* \n\n𝚂𝚄𝙱 𝙱𝙾𝚃𝚂 𝙰𝙲𝚃𝙸𝚅𝙾𝚂 : ${totalUsers || '0'}\n\n${replyMessage.trim()}`.trim();
+const responseMessage = `*CARGANDO LA LISTA DE BOTS ACTIVOS*\n\n\n █▒▒▒▒▒▒▒▒▒10%\n ████▒▒▒▒▒▒30%\n █████▒▒▒▒▒50%\n ████████▒▒80%\n ██████████100%\n\n *ESTA ES LA LISTA DE SUBOTS ACTIVOS🌷* \n\n𝚂𝚄𝙱 𝙱𝙾𝚃𝚂 𝙰𝙲𝚃𝙸𝚅𝙾𝚂 : ${totalUsers || '0'}\n\n${replyMessage.trim()}`.trim();
 await _envio.sendMessage(m.chat, {text: responseMessage, mentions: _envio.parseMention(responseMessage)}, {quoted: m})}
 handler.command = handler.help = ['listjadibot', 'bots', 'subsbots'];
 handler.tags = ['jadibot'];
