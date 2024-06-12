71cc1ad566d85404ef7d471b08a39e08868563a1
Aldair-ld <frankaldair29@gmail.com>
2024-06-11 22:37:13 -0500

    Update rpg-retirar.js

--- plugins/rpg-retirar.js
+++ plugins/rpg-retirar.js
@@ -2,19 +2,19 @@ import db from '../lib/database.js';
 
 let handler = async (m, { args }) => {
    
-   let user =  global.db.data.users[m.sender]; if (!args[0]) return m.reply('*[❌] Ingresa la cantidad de dinero que deseas Retirar.*');
+   let user =  global.db.data.users[m.sender]; if (!args[0]) return m.reply('*Ingresa la cantidad de diamante que deseas Retirar.*');
    if (args[0] == '--all') {
-      let count = parseInt(user.bank);
-      user.bank -= count * 1
+      let count = parseInt(user.limit);
+      user.limit -= count * 1
       user.dolares += count * 1
-      await m.reply(`*Retiraste ${count} de dinero del Banco.* 💸`);
+      await m.reply(`*Retiraste ${limit} diamantes del Banco.*`);
       return !0
    }
    if (!Number(args[0])) return m.reply('[⚠️] La cantidad debe ser un Numero.');
    let count = parseInt(args[0]);
-   if (!user.bank) return m.reply('[😿] No tienes dinero en Banco.')
-   if (user.bank < count) return m.reply(`[❌] Solo tienes ${user.bank} de dinero en el Banco.`)
-   user.bank -= count * 1
+   if (!user.limit) return m.reply('[😿] No tienes suficiente diamantes en el Banco.')
+   if (user.limit < count) return m.reply(`[❌] Solo tienes ${user.limit} de diamantes en el Banco.`)
+   user.limit -= count * 1
    user.dolares += count * 1
    await m.reply(`*Retiraste ${count} de dinero del Banco.* 💸`)
 }
